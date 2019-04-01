9381735c1898d1a8e369300057dcdfc3ca1c63ab
Nathan Hoad <nathan@nathanhoad.net>
2019-04-01 13:46:13 +1000

    Fixed over-zealous embed replacer

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const d3 = require('./d3');
 const GML = require('./loader');
 
 const PROJECT_NAME = 'cyclone-tracker';
-const BASE_URL = `https://www.abc.net.au/res/sites/news-projects/cyclone-tracker/3.0.19/`;
+const BASE_URL = `https://www.abc.net.au/res/sites/news-projects/cyclone-tracker/3.0.20/`;
 
 const getDistId = strings => {
   if (typeof strings === 'string') strings = [strings];
@@ -26,39 +26,45 @@ const getDistId = strings => {
 function init() {
   const App = require('./components/App');
 
-  [].slice.call(document.querySelectorAll(`a[href*="/news/specials/cyclones"]`)).forEach(a => {
-    // Create an iframe to house an instance of ourselves in it
-    const iframe = document.createElement('iframe');
-    const distId = getDistId([a.href, document.location.search]);
-    if (distId) {
-      iframe.src = `${BASE_URL}?cyclone=${distId}`;
-    } else {
-      iframe.src = BASE_URL;
-    }
-    iframe.width = a.parentElement.offsetWidth;
-    iframe.height = 500;
-    iframe.setAttribute('frameBorder', 0);
-
-    // Find the nearest parent with the extended link embed class
-    const replace = a.closest('.view-external-link-embedded') || a;
-    replace.parentElement.insertBefore(iframe, replace);
-    replace.parentElement.removeChild(replace);
-
-    // Stop the page from moving while scrolling on mobile
-    window.addEventListener('message', event => {
-      if (typeof event.data.lockScroll !== 'undefined') {
-        if (event.data.lockScroll) {
-          document.body.style.setProperty('overflow', 'hidden');
-          document.body.style.setProperty('width', '100%');
-          document.body.style.setProperty('height', '100%');
-        } else {
-          document.body.style.removeProperty('overflow');
-          document.body.style.removeProperty('width');
-          document.body.style.removeProperty('height');
-        }
+  [].slice
+    .call(
+      document.querySelectorAll(
+        `.embed-content a[href*="/news/specials/cyclones"], .inline-content a[href*="/news/specials/cyclones"]`
+      )
+    )
+    .forEach(a => {
+      // Create an iframe to house an instance of ourselves in it
+      const iframe = document.createElement('iframe');
+      const distId = getDistId([a.href, document.location.search]);
+      if (distId) {
+        iframe.src = `${BASE_URL}?cyclone=${distId}`;
+      } else {
+        iframe.src = BASE_URL;
       }
+      iframe.width = a.parentElement.offsetWidth;
+      iframe.height = 500;
+      iframe.setAttribute('frameBorder', 0);
+
+      // Find the nearest parent with the extended link embed class
+      const replace = a.closest('.view-external-link-embedded') || a;
+      replace.parentElement.insertBefore(iframe, replace);
+      replace.parentElement.removeChild(replace);
+
+      // Stop the page from moving while scrolling on mobile
+      window.addEventListener('message', event => {
+        if (typeof event.data.lockScroll !== 'undefined') {
+          if (event.data.lockScroll) {
+            document.body.style.setProperty('overflow', 'hidden');
+            document.body.style.setProperty('width', '100%');
+            document.body.style.setProperty('height', '100%');
+          } else {
+            document.body.style.removeProperty('overflow');
+            document.body.style.removeProperty('width');
+            document.body.style.removeProperty('height');
+          }
+        }
+      });
     });
-  });
 
   [].slice.call(document.querySelectorAll(`[data-${PROJECT_NAME}-root]`)).forEach((root, index) => {
     if (document.location.search && document.location.search.indexOf('cyclone') > -1) {
